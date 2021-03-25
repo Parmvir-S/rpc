d8c0bb757de17aedfac4692682265621006a0752
Parmvir-S <parmvir.shergill@gmail.com>
2021-03-24 22:31:16 -0700

    changed prompt to confirm - also changed else if statements

--- rpc.js
+++ rpc.js
@@ -15,11 +15,13 @@ let compScore = 0;
 //Update User Score and Computer Score
 let updateUserScore = () => {
     userScore += 1;
+    alert(`You win, your score is ${userScore} and the computers score is ${compScore}`);
     return userScore
 }
 
 let updateCompScore = () => {
     compScore += 1;
+    alert(`You lose, your score is ${userScore} and the computers score is ${compScore}`);
     return compScore
 }
 
@@ -30,22 +32,16 @@ let gameplay = (compChoice) => {
         alert(`It's a tie, your score is ${userScore} and the computers score is ${compScore}`);
     } else if (userInput === "rock" && compChoice() === "scissors") {
         updateUserScore();
-        alert(`You win, your score is ${userScore} and the computers score is ${compScore}`);
     } else if (userInput === "rock" && compChoice() === "paper") {
         updateCompScore();
-        alert(`You lose, your score is ${userScore} and the computers score is ${compScore}`);
     } else if (userInput === "paper" && compChoice() === "rock") {
         updateUserScore();
-        alert(`You win, your score is ${userScore} and the computers score is ${compScore}`);
     } else if (userInput === "paper" && compChoice() === "scissors") {
         updateCompScore();
-        alert(`You lose, your score is ${userScore} and the computers score is ${compScore}`);
     } else if (userInput === "scissors" && compChoice() === "paper") {
         updateUserScore();
-        alert(`You win, your score is ${userScore} and the computers score is ${compScore}`);
     } else if (userInput === "scissors" && compChoice() === "rock") {
         updateCompScore();
-        alert(`You lose, your score is ${userScore} and the computers score is ${compScore}`);
     }
 }
 
@@ -56,10 +52,10 @@ while (userScore < 5 || compScore < 5) {
         (userScore === 5) ? alert("You Win The Game"): alert("You Lose the Game");
             userScore = 0;
             compScore = 0;
-            let play = prompt("Wanna play again? ").toLowerCase();
-            if (play === 'yes') {
+            let play = confirm("Wanna play again? ");
+            if (play === true) {
                 gameplay(compChoice);
-            } else if (play === 'no') {
+            } else if (play === false) {
                 alert("Thanks for playing");
                 break
             }
